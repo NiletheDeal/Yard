dea020687d6d5fe45ee7de29244f984a67764a63
U-BSD\420607 <420607@SHS-GYBF6X3.beaverton.k12.or.us>
2024-03-01 13:52:00 -0800

    comment idea

--- main.cpp
+++ main.cpp
@@ -15,5 +15,6 @@ int main() {
     if(strcmp(command, "INPUT") == 0) {
       cout << "Enter your input" << endl;
       cin.getline(input, 10000);
-      
+      //queue is a linked list where you save the head and tail, still single link(maybe a method to find the last node(which node points to null)
+      //stack is a linked list where you save only the head(bottom of stack), still single link (find the last node that points to not null and then that is the top of stack;
       
