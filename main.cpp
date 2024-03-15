eeb970ecb40c86072cdc60d5f3d6109501da35eb
U-BSD\420607 <420607@SHS-GYBF6X3.beaverton.k12.or.us>
2024-03-15 12:29:06 -0700

    Works just needs comments

--- main.cpp
+++ main.cpp
@@ -1,3 +1,7 @@
+//Neel Madala
+//3/15/12024
+//This program takes in a function puts it through the shunting yard alogrithm and then into a binary expression tree. Then allows the user to print out their function in Infix, Postfix, or Prefix notation
+
 #include <iostream>
 #include <cstring>
 #include <cctype>
@@ -23,7 +27,7 @@ int main() {
   Node* front = NULL;
   Node* rear = NULL;
   char value;
-  while(looping == true) {
+  while(looping == true) {//using the program
     cout << "INPUT or QUIT" << endl;
     cin.get(command, 10);
     cin.clear();
@@ -32,45 +36,46 @@ int main() {
       cout << "Enter your input" << endl;
       cin.getline(input, 100);
       cin.clear();
-      cout << input << endl;
-      //good input
       cout << strlen(input) << endl;
       top = NULL;
       front = NULL;
       rear = NULL;
-      for(int i = 0 ; i < strlen(input); i++) {
-	if(input[i] != ' ') {
-	  if(isdigit(input[i])) {
-	    enqueue(front, rear, input[i]);
+      for(int i = 0 ; i <= strlen(input); i++) {//goes through each char in the input
+	if(input[i] != ' ') {//Not a  space
+	  if(isdigit(input[i])) {//if a number add to queue
 	    cout << input[i] << endl;
+	    enqueue(front, rear, input[i]);
 	  }
 	  else {
 	    int inP = precedence(input[i]);
-	    if(top == NULL) {
+	    if(top == NULL) {//if the stack is empty add value to it
 	      cout << input[i] << endl;
 	      push(top, input[i]);
 	    }
-	    else {
-	      cout << input[i] << endl;
+	    else {//if the stack has something on it
 	      int topP = precedence(top->getData());
-	      if(inP  == 1 || inP == 2 || inP == 3) {//operators precedence check
+	      if(inP  == 1 || inP == 2 || inP == 3) {//operators precedence check if the operator is greater precedence then what you are gonna add to it in which case move the top of the stack to the queue until it is less or you reach (
 		if(top != NULL) {
 		  while(topP >= inP && top->getData() != '(') {
 		    topP = precedence(top->getData());
+		    
 		    enqueue(front, rear, top->getData());
 		    pop(top);
-		    if(top ==NULL) {
+		    if(top ==NULL) {//end of list stop
 		      break;
 		    }
 		  }
 		}
-	      push(top, input[i]);//Add the input to the stack  
+		cout << input[i] << endl;
+		push(top, input[i]);//Add the input to the stack  
 	      }
 	      else if(inP == 5) {//left parenthesises just add
+		cout << input[i] << endl;
 		push(top, input[i]);
 	      }
 	      else if(inP == 6) {//right parenthesises pop the stack till the matched parentheseses
 		while(top->getData() != '(') {
+		  cout << "This isn't happening" << endl;
 		  enqueue(front, rear, top->getData());
 		  pop(top);
 		}
@@ -85,27 +90,33 @@ int main() {
       
       
       while(top != NULL) {//move the stack to the queue at the end
+	cout << "test4" << top->getData() << endl;
 	enqueue(front, rear, top->getData());
 	pop(top);
       }
+      cout << "Queue is: " << endl;
       displayQueue(front, rear);
       Node* tree = NULL;//top of the tree stack
       while(front != rear) {
 	value = front->getData();
 	int vP = precedence(value);
-	if(vP = 0) {//if the value is a number
-	  push(tree, value);
+	if(vP == 0) {//if the value is a number
+	  cout << "test" << endl;
+	  push(tree, value);//just push it to the tree
 	}
 	else {
 	  Node* temp = new Node();
 	  temp->setData(value);
+	  //tree->setNext(NULL);
 	  temp->setRight(tree);
 	  pop(tree);
+	  //tree->setNext(NULL);
 	  temp->setLeft(tree);
 	  pop(tree);
 	  pushTree(tree, temp);
 	}
 	front = front->getNext();
+	print(tree, 0);
       }
       //Rear Node
       value = rear->getData();
@@ -133,14 +144,17 @@ int main() {
 	cin.ignore(10000, '\n');
 	if(strcmp(command, "IN") == 0) {
 	  disInfix(tree);
+	  cout << endl << endl;
 	  check = true;
 	}
 	else if(strcmp(command, "POST") == 0) {
 	  disPostfix(tree);
+	  cout << endl << endl;
 	  check =true;
 	}
 	else if(strcmp(command, "PRE") == 0) {
 	  disPrefix(tree);
+	  cout << endl << endl;
 	  check = true;
 	}
 	else {
@@ -195,7 +209,7 @@ bool isEmpty(Node* front, Node*rear) {
 }
 void pop(Node*& top) {//remove the top node
   if(top == NULL) {
-    cout << "Empty" << endl;
+    cout << "Empty1" << endl;
   }
   else {
     Node* temp = top;
@@ -235,33 +249,36 @@ void pushTree(Node*& top, Node* tree) {//gonna add a node to the tree stack inst
 }
 //Display tree functions
 void disInfix(Node* InF) { //Display Infix Function
-    if (InF != NULL) {
-        if (precedence(InF->getData()) != 0) {
-            cout << "( ";
-        }
-        disInfix(InF->getLeft());
-        cout << InF->getData() << " ";
-        disInfix(InF->getRight());
-        if (precedence(InF->getData()) != 0) {
-            cout << ") ";
-        }
-    }
+  if (InF->getLeft() != NULL) {
+    disInfix(InF->getLeft());
+  }
+  cout << InF->getData();
+  if(InF->getRight() != NULL) {
+    disInfix(InF->getRight());
+  }
+  return;
 }
 
 void disPrefix(Node* PreF) { //Display Prefix Function
-    if (PreF != NULL) {
-        cout << PreF->getData() << " ";
-        disPrefix(PreF->getLeft());
-        disPrefix(PreF->getRight());
-    }
+  cout << PreF->getData();
+  if(PreF->getLeft() != NULL) {
+    disPrefix(PreF->getLeft());
+  }
+  if(PreF->getRight() != NULL) {
+    disPrefix(PreF->getRight());
+  }
+  return;
 }
 
 void disPostfix(Node* PosF) { //Display Postfix Function
-    if (PosF != NULL) {
+  if (PosF->getLeft() != NULL) {
         disPostfix(PosF->getLeft());
-        disPostfix(PosF->getRight());
-        cout << PosF->getData() << " ";
-    }
+  }
+  if(PosF->getRight() != NULL) {
+    disPostfix(PosF->getRight());
+  }
+  cout << PosF->getData();
+  return;
 }
 void displayQueue(Node* front, Node* rear) { //DisplayQueue function
     if (isEmpty(front, rear)) {
